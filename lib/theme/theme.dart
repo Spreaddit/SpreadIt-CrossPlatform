3eb1e4ed78fdcfe57514c0c8fa55303ec77e0c8b
Farida Mukhtar <farida.moukhtar03@eng-st.cu.edu.eg>
2024-03-18 22:40:23 +0200

    Update theme.dart

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -5,8 +5,11 @@ const redditBlack = Color(0x00000000);
 const redditGrey = Color.fromRGBO(206, 227, 248, 1);
 
 ThemeData spreadItTheme = ThemeData(
+    primaryColor: redditBlack,
+    primaryColorLight: redditGrey,
+    primaryColorDark: redditBlack,
     colorScheme: ColorScheme.fromSeed(
-  seedColor: redditOrange,
-  primary: redditOrange,
-  secondary: redditGrey,
-));
+      seedColor: redditOrange,
+      primary: redditOrange,
+      secondary: redditGrey,
+    ));
