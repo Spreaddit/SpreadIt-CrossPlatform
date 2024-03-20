e1884e09749a87ef8a9b4cca2a8c3b2cc9656686
Chahd ElZalaki <shahd.zalaky@gmail.com>
2024-03-20 23:38:01 +0200

    fixed URL

--- lib/features/reset_password/data/update_password.dart
+++ lib/features/reset_password/data/update_password.dart
@@ -4,7 +4,7 @@ import 'package:dio/dio.dart';
 void UpdatePassword(String currentPassword, String newPassword)async{
   try{
     final response = await Dio().post(
-      "http://10.0.2.2:3001/AMIRAELGARF02/Spreadit1/1.0.0",
+      "http://10.0.2.2:3001/AMIRAELGARF02/Spreadit1/1.0.0/reset-password",
        data: {'currentPassword': currentPassword, 'newPassword': newPassword}
        );
     if (response.statusCode == 200) {
