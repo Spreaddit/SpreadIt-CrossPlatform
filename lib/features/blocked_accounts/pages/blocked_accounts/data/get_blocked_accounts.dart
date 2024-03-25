566583abcbbc898b71fd767fc58e6efdeed6b85c
Rehab Ahmed <rehabfecky@gmail.com>
2024-03-25 20:30:33 +0200

    documetation+widget test

--- lib/features/blocked_accounts/pages/blocked_accounts/data/get_blocked_accounts.dart
+++ lib/features/blocked_accounts/pages/blocked_accounts/data/get_blocked_accounts.dart
@@ -1,6 +1,25 @@
 import 'package:dio/dio.dart';
 import 'package:spreadit_crossplatform/api.dart';
 
+/// Fetches the list of blocked accounts from the server.
+///
+/// This function sends a GET request to the server to retrieve the list of blocked accounts.
+/// It expects the server to respond with a JSON object containing a key 'blockedAccounts',
+/// which holds the list of blocked accounts.
+///
+/// If the request is successful (HTTP status code 200), it returns the list of blocked accounts.
+/// If the request fails or encounters an error, it returns an empty list.
+///
+/// Throws an exception if there is an error during the network request.
+///
+/// Example usage:
+///
+/// ```dart
+/// var blockedAccounts = await getBlockedAccounts();
+/// ```
+///
+/// Returns a Future that completes with a list of blocked accounts.
+
 Future<List> getBlockedAccounts() async {
   try {
     print("gowa blocked accounts");
--- lib/features/blocked_accounts/pages/blocked_accounts/presentation/blocked_accounts_page.dart
+++ lib/features/blocked_accounts/pages/blocked_accounts/presentation/blocked_accounts_page.dart
@@ -3,7 +3,9 @@ import 'package:spreadit_crossplatform/features/Account_Settings/presentation/wi
 import 'package:spreadit_crossplatform/features/blocked_accounts/pages/blocked_accounts/data/get_blocked_accounts.dart';
 import 'package:spreadit_crossplatform/features/blocked_accounts/pages/blocked_accounts/data/put_blocked_accounts.dart';
 
+/// Widget for displaying a list of blocked accounts with an option to unblock them.
 class BlockedAccountsPage extends StatefulWidget {
+  /// Constructs a 'BlockedAccountsPage'.
   const BlockedAccountsPage({Key? key}) : super(key: key);
   @override
   State<BlockedAccountsPage> createState() {
@@ -11,10 +13,14 @@ class BlockedAccountsPage extends StatefulWidget {
   }
 }
 
+/// State class for [BlockedAccountsPage].
 class _BlockedAccountsPageState extends State<BlockedAccountsPage> {
+  /// List of blocked accounts.
   List<dynamic> blockedAccountsList = [];
 
   @override
+
+  /// Sets the initial state of the page.
   void initState() {
     super.initState();
     setState(() {
@@ -22,6 +28,7 @@ class _BlockedAccountsPageState extends State<BlockedAccountsPage> {
     });
   }
 
+  /// Fetches the list of blocked accounts from the network.
   Future<void> fetchData() async {
     var data = await getBlockedAccounts();
     setState(() {
@@ -35,6 +42,7 @@ class _BlockedAccountsPageState extends State<BlockedAccountsPage> {
       appBar: SettingsAppBar(title: "Blocked Accounts"),
       body: blockedAccountsList.isEmpty
           ? Center(
+              key: Key('EmptyStateImage'),
               child: Image.asset('assets/images/Empty_Toast.png', width: 200))
           : ListView(
               shrinkWrap: true,
