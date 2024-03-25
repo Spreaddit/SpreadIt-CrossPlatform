81b0e085a1e637344301aa54211380b16598b3bd
Farida Mukhtar <farida.moukhtar03@eng-st.cu.edu.eg>
2024-03-25 14:26:49 +0200

    authentication validations

--- test/forget_password_widget_test.dart
+++ test/forget_password_widget_test.dart
@@ -2,17 +2,41 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:spreadit_crossplatform/features/forget_password/presentation/pages/forget_password_main.dart';
 
-void main() {
-  testWidgets('forget password widget test', (WidgetTester tester) async {
-    await tester.pumpWidget(ForgetPassword());
-    await tester.pumpAndSettle();
-    var textField = find.byType(TextField);
-    expect(textField, findsOneWidget);
-    await tester.enterText(textField, "chz@gmail.com");
-    expect(find.text("chz@gmail.com"), findsOneWidget);
-    var button = find.text("Reset Password");
-    expect(button, findsOneWidget);
-    await tester.tap(button);
-    await tester.pumpAndSettle();
-  });
+
+
+class CustomBindings extends AutomatedTestWidgetsFlutterBinding {
+  @override
+  bool get overrideHttpClient => false;
 }
+
+void main(){
+  CustomBindings();
+  testWidgets('forget password widget test', (WidgetTester tester) async {
+
+    await tester.pumpWidget(MaterialApp(home: Scaffold(body:ForgetPassword())));
+    await tester.pumpAndSettle();
+    // inspect header title
+    expect(find.text('Forgot password?'), findsOneWidget);
+    // inspect text 
+    expect(find.text("Enter your email address or username and we'll send you a link to reset your password"), findsOneWidget);
+    // inspcet button
+    expect(find.text('Reset Password'), findsOneWidget);
+    // inspect input field
+    expect(find.byType(TextField), findsOneWidget);
+
+    // Tap on the Reset Password button without entering any input.
+    await tester.tap(find.text('Reset Password'));
+    await tester.pump();
+    // Expect a snackbar with error message because no input is provided.
+    expect(find.text('please enter your email or username'), findsOneWidget);
+
+    // Enter valid input.
+    await tester.enterText(find.byType(TextField), 'test@example.com');
+    await tester.pumpAndSettle();
+   // expect(find.text("an email was sent to you to reset your password"), findsOneWidget);
+    
+    
+  });
+
+  }
+  
--- test/reset_password_widget_test.dart
+++ test/reset_password_widget_test.dart
@@ -1,25 +1,92 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:spreadit_crossplatform/features/generic_widgets/custom_input.dart';
 import 'package:spreadit_crossplatform/features/reset_password/presentation/pages/reset_password_main.dart';
 
+class CustomBindings extends AutomatedTestWidgetsFlutterBinding {
+  @override
+  bool get overrideHttpClient => false;
+}
+
 void main() {
+  CustomBindings();
   testWidgets('reset password widget test', (WidgetTester tester) async {
-    await tester.pumpWidget(ResetPassword());
-    var currentPassTextField =
-        find.widgetWithText(TextField, "Current password");
-    expect(currentPassTextField, findsExactly(2));
-    var newPassTextField = find.widgetWithText(TextField, "New password");
-    expect(newPassTextField, findsExactly(2));
-    var confirmedPassTextField =
-        find.widgetWithText(TextField, "Confirmed password");
-    expect(confirmedPassTextField, findsExactly(2));
-    var saveButton = find.widgetWithText(Container, "Save");
-    expect(saveButton, findsExactly(2));
-    await tester.enterText(currentPassTextField, "Hello1234");
-    expect(find.text("Hello1234"),
-        findsAny); // because the user could actually reset his new password with his current password
-    await tester.enterText(newPassTextField, "HelloWorld12");
-    await tester.enterText(confirmedPassTextField, "HelloWorld12");
-    expect(find.text("HelloWorld12"), findsExactly(2));
+    String currentPassword = '';
+    String newPassword = '';
+    String confirmedPassword = '';
+
+    await tester.pumpWidget(MaterialApp(home: Scaffold(body: ResetPassword())));
+    await tester.pumpAndSettle();
+
+    // inspect current password input
+    var currentPassField = find.descendant(
+      of: find.widgetWithText(CustomInput, "Current password"),
+      matching: find.byType(TextField),
+    );
+    expect(currentPassField, findsOneWidget);
+
+    // inspect new password input
+    var newPassField = find.descendant(
+      of: find.widgetWithText(CustomInput, "New password"),
+      matching: find.byType(TextField),
+    );
+    expect(newPassField, findsOneWidget);
+
+    // inspect confirmed password input
+    var confirmedPassField = find.descendant(
+      of: find.widgetWithText(CustomInput, "Confirmed password"),
+      matching: find.byType(TextField),
+    );
+    expect(confirmedPassField, findsOneWidget);
+
+    // inspect save button
+    expect(find.text("Save"), findsOneWidget);
+    // inspect forget password button
+    expect(find.text("Forgot password"), findsOneWidget);
+
+    // Tap on the Save button without entering any input.
+    await tester.tap(find.text('Save'));
+    await tester.pump();
+    // Expect a snackbar with error message because no input is provided.
+    expect(find.text("provide your current password"), findsOneWidget);
+
+    // check the passwords' length
+    await tester.enterText(currentPassField, 'Hello');
+    currentPassword =
+        tester.widget<TextField>(currentPassField).controller!.text;
+    await tester.enterText(newPassField, "Hello1234");
+    newPassword = tester.widget<TextField>(newPassField).controller!.text;
+    await tester.enterText(currentPassField, "Hello123456");
+    confirmedPassword =
+        tester.widget<TextField>(confirmedPassField).controller!.text;
+    await tester.tap(find.text('Save'));
+    await tester.pumpAndSettle();
+    //expect(find.text("password must contain more than 8 characters"), findsOneWidget);
+
+    // check that the new password and the confirmed passwords are identical
+    await tester.enterText(currentPassField, 'HelloThere');
+    currentPassword =
+        tester.widget<TextField>(currentPassField).controller!.text;
+    await tester.enterText(newPassField, "Hello1234");
+    newPassword = tester.widget<TextField>(newPassField).controller!.text;
+    await tester.enterText(currentPassField, "Hello123456");
+    confirmedPassword =
+        tester.widget<TextField>(confirmedPassField).controller!.text;
+    await tester.tap(find.text('Save'));
+    await tester.pumpAndSettle();
+    //expect(find.text("password mismatch:please reconfirm your new password"), findsOneWidget);
+
+    // inspect when all inputs are valid
+    await tester.enterText(currentPassField, 'HelloThere');
+    currentPassword =
+        tester.widget<TextField>(currentPassField).controller!.text;
+    await tester.enterText(newPassField, "Hello1234");
+    newPassword = tester.widget<TextField>(newPassField).controller!.text;
+    await tester.enterText(currentPassField, "Hello1234");
+    confirmedPassword =
+        tester.widget<TextField>(confirmedPassField).controller!.text;
+    await tester.tap(find.text('Save'));
+    await tester.pumpAndSettle();
+    //expect(find.text("password is updated successfully"),findsOneWidget);
   });
 }
