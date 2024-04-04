74c5e5270659eeacf4fce747983bdc93e331c8a9
Basem1166 <basem_elgalfy@yahoo.com>
2024-04-04 05:46:00 +0200

    created the API for the get_specific_category

---
+++ lib/features/discover_communities/data/get_specific_category.dart
@@ -0,0 +1,32 @@
+import 'community.dart';
+import 'package:dio/dio.dart';
+import 'package:spreadit_crossplatform/api.dart';
+
+class GetSpecificCommunity {
+  Dio dio = Dio();
+
+  Future<List<Community>> getCommunities(String category) async {
+    try {
+      Response response = await dio.get(
+        '$apiUrl/community/get-specific-category',
+        queryParameters: {
+          'category': category,
+        },
+      );
+
+      if (response.statusCode == 200) {
+        List<Community> communities =
+            (response.data as List).map((i) => Community.fromJson(i)).toList();
+        return communities;
+      } else if (response.statusCode == 404) {
+        throw Exception('No communities found for the specified category');
+      } else if (response.statusCode == 500) {
+        throw Exception('Internal server error');
+      } else {
+        throw Exception('Bad request, invalid request paramters');
+      }
+    } catch (e) {
+      throw Exception('Failed to load communities: $e');
+    }
+  }
+}
