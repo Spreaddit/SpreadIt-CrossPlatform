fbd5515547276270a57148909c7a813646b59b45
Basem1166 <basem_elgalfy@yahoo.com>
2024-04-04 17:49:54 +0200

    added a horizontal scrollable section

--- lib/features/discover_communities/presentation/pages/discover_communities.dart
+++ lib/features/discover_communities/presentation/pages/discover_communities.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'category.dart';
+import '../widgets/subreddit_cards.dart';
 
 class DiscoverCommunitiesPage extends StatelessWidget {
   final List<String> categories = [];
@@ -40,47 +41,80 @@ class DiscoverCommunitiesBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: categories.length,
-      itemBuilder: (context, index) {
-        if (index == 0) {
-          return ListTile(
-            title: Text(
-              categories[index],
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-          );
-        } else {
-          return TextButton(
-            style: TextButton.styleFrom(
-              shape: RoundedRectangleBorder(),
-              backgroundColor: Colors.transparent,
-              foregroundColor: Colors.black87,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  categories[index],
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    color: Colors.black87,
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          // First section with horizontal scrolling
+          Container(
+            height: 160.0, // Set a height for the horizontal scrolling section
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: <Widget>[
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  child: SubredditCard(
+                    index: 1,
+                    title: 'Title',
+                    description: 'Description',
+                    numberOfMembers: '1000',
+                    image: 'assets/flutterdev.png',
                   ),
                 ),
-                Icon(Icons.arrow_right, color: Colors.black87),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  child: SubredditCard(
+                    index: 2,
+                    title: 'Title',
+                    description: 'Description',
+                    numberOfMembers: '1000',
+                    image: 'assets/flutterdev.png',
+                  ),
+                ),
               ],
             ),
-            onPressed: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) =>
-                        CategoryPage(categoryName: categories[index]),
-                  ));
+          ),
+          // Second section with vertical scrolling
+          ListView.builder(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            itemCount: categories.length,
+            itemBuilder: (context, index) {
+              if (index == 0) {
+                return ListTile(
+                  title: Text(
+                    categories[index],
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                );
+              } else {
+                return TextButton(
+                  style: TextButton.styleFrom(
+                    shape: RoundedRectangleBorder(),
+                    backgroundColor: Colors.transparent,
+                    foregroundColor: Colors.black87,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        categories[index],
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          color: Colors.black87,
+                        ),
+                      ),
+                      Icon(Icons.arrow_right, color: Colors.black87),
+                    ],
+                  ),
+                  onPressed: () {
+                    // Add your navigation logic here
+                  },
+                );
+              }
             },
-          );
-        }
-      },
+          ),
+        ],
+      ),
     );
   }
 }
