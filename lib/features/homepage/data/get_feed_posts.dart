da812b3334cc31c262dbe85d111848a0c45bffcd
Farida Mukhtar <farida.moukhtar03@eng-st.cu.edu.eg>
2024-04-08 00:50:49 +0200

    Update get feed posts to render different categories for both homepage and community

--- lib/features/homepage/data/get_feed_posts.dart
+++ lib/features/homepage/data/get_feed_posts.dart
@@ -10,32 +10,64 @@ enum PostCategories {
   top,
   random,
   recent,
+  views,
 }
 
 /// takes [PostCategories] as a parameter and
 /// returns its respective endpoint
-String postCategoryEndpoint(PostCategories action) {
-  switch (action) {
-    case PostCategories.best:
-      return "/best/best/";
-    case PostCategories.hot:
-      return "/best/hot/";
-    case PostCategories.newposts:
-      return "/best/new/";
-    case PostCategories.top:
-      return "/best/top/";
-    case PostCategories.random:
-      return "/best/random/";
-    case PostCategories.recent:
-      return "/best/recent-posts/";
+String postCategoryEndpoint({
+  required PostCategories action,
+  String? subspreaditName,
+  String? timeSort = "",
+}) {
+  if (subspreaditName != null) {
+    switch (action) {
+      case PostCategories.best:
+        return "/best/";
+      case PostCategories.hot:
+        return "/hot/";
+      case PostCategories.newposts:
+        return "/new/";
+      case PostCategories.top:
+        return "/top/";
+      case PostCategories.recent:
+        return "/best/recent-posts/";
+      case PostCategories.views:
+        return "/sort/views/";
+      default:
+        return "";
+    }
+  } else {
+    switch (action) {
+      case PostCategories.hot:
+        return "/subspreadit/$subspreaditName/hot/";
+      case PostCategories.newposts:
+        return "/subspreadit/$subspreaditName/new/";
+      case PostCategories.top:
+        return "/subspreadit/$subspreaditName/top/$timeSort";
+      case PostCategories.random:
+        return "/subspreadit/$subspreaditName/random/";
+      default:
+        return "";
+    }
   }
 }
 
 /// Takes [PostCategories] as a paremeter
 /// and fetches its respective [Post] List
-Future<List<Post>> getFeedPosts(PostCategories category) async {
+Future<List<Post>> getFeedPosts({
+  required PostCategories category,
+  String? subspreaditName,
+  String? timeSort = "",
+}) async {
   try {
-    String requestURL = apiUrl + postCategoryEndpoint(category);
+    String requestURL = apiUrl +
+        postCategoryEndpoint(
+          action: category,
+          subspreaditName: subspreaditName,
+          timeSort: timeSort,
+        );
+
     final response = await Dio().get(requestURL);
     if (response.statusCode == 200) {
       return (response.data as List).map((x) => Post.fromJson(x)).toList();
