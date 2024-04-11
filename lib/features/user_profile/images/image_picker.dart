179d0dcc01338336c9630d8ac1a237b3c848f74f
mariammahrous23 <mariammahrous23@gmail.com>
2024-04-11 18:31:26 +0200

    Image picker update

--- lib/features/user_profile/images/image_picker.dart
+++ lib/features/user_profile/images/image_picker.dart
@@ -1,9 +1,14 @@
+import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'dart:io';
 import 'package:image_picker/image_picker.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:file_picker/file_picker.dart';
 
 
+
+// Function to pick image from the device's gallery using ImagePicker
 Future<File?> pickImageFromFilePicker() async {
   try {
     final image = await ImagePicker().pickImage(source: ImageSource.gallery);
@@ -17,13 +22,27 @@ Future<File?> pickImageFromFilePicker() async {
   }
 }
 
-
-  ImageProvider selectImage(File? ImageFile , String? ImageURL) {
-    if (ImageFile != null) {
-      return FileImage(ImageFile);
-    } else if (ImageURL != null && ImageURL.isNotEmpty) {
-      return NetworkImage(ImageURL);
-    } else {
-      return NetworkImage('https://addlogo.imageonline.co/image.jpg');
-    }
+// Function to pick image from the file system using HTML input element
+Future<Uint8List?> pickImageFromFilePickerWeb() async {
+  if(kIsWeb)
+ { 
+  FilePickerResult? result = await FilePicker.platform.pickFiles();
+if (result != null) {
+  print(result.files.single.bytes);
+  return result.files.single.bytes;
+}
   }
+
+}
+
+ImageProvider<Object> selectImage(File? imageFile, String? imageURL, Uint8List? imageWeb) {
+  if (imageWeb != null && kIsWeb) {
+    return MemoryImage(imageWeb);
+  } else if (imageFile != null) {
+    return FileImage(imageFile);
+  } else if (imageURL != null && imageURL.isNotEmpty) {
+    return NetworkImage(imageURL);
+  } else {
+    return NetworkImage('https://addlogo.imageonline.co/image.jpg');
+  }
+}
