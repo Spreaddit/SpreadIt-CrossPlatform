06a40cef9e3db77a1a6972b8fca2ffcc9fa3779f
chiz8622 <128740994+chiz8622@users.noreply.github.com>
2024-04-14 17:25:28 +0200

    Update rules_page_widget_test.dart

--- test/create_post_tests/rules_page_widget_test.dart
+++ test/create_post_tests/rules_page_widget_test.dart
@@ -1,42 +1,50 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:spreadit_crossplatform/features/create_post/presentation/pages/rules_page.dart';
-// import 'package:spreadit_crossplatform/features/discover_communities/data/community.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:spreadit_crossplatform/features/create_post/presentation/pages/rules_page.dart';
+import 'package:spreadit_crossplatform/features/discover_communities/data/community.dart';
 
-// void main() {
-//   testWidgets('CommunityRules widget test', (WidgetTester tester) async {
-//     await tester.pumpWidget(MaterialApp(
-//       home: CommunityRules(
-//         communityRules: [
-//           Rule(
-//             id:0,
-//             title: 'Rule 1',
-//             description: 'Description 1',
-//           ),
-//           {'title': 'Rule 2', 'description': 'Description 2'},
-//         ],
-//       ),
-//     ));
+void main() {
+  testWidgets('CommunityRules widget test', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+      home: CommunityRules(
+        communityRules: [
+          Rule(
+            id:'0',
+            title: 'Rule 1',
+            description: 'Description 1',
+            reportReason: '',
+            communityName: 'r/AskReddit'
+          ),
+          Rule(
+            id: '1',
+            title: 'Rule 2', 
+            description: 'Description 2',
+            reportReason: '',
+            communityName: 'r/AskReddit'
+          ),
+        ],
+      ),
+    ));
 
-//     //check that the header is present
-//     expect(find.text('Community rules'), findsOneWidget);
-//     expect(
-//         find.text(
-//             'Rules are different for each community. Reviewing the rules can help you be more successful when posting or commenting on this community'),
-//         findsOneWidget);
+    //check that the header is present
+    expect(find.text('Community rules'), findsOneWidget);
+    expect(
+        find.text(
+            'Rules are different for each community. Reviewing the rules can help you be more successful when posting or commenting on this community'),
+        findsOneWidget);
 
-//     //Verify if all the rules are displayed
-//     expect(find.text('Rule 1'), findsOneWidget);
-//     expect(find.text('Rule 2'), findsOneWidget);
+    //Verify if all the rules are displayed
+    expect(find.text('Rule 1'), findsOneWidget);
+    expect(find.text('Rule 2'), findsOneWidget);
 
-//     //Verify that the arrows work
-//     expect(find.byIcon(Icons.keyboard_arrow_down), findsExactly(2));
-//     await tester.tap(find.text('Rule 1'));
-//     await tester.pump();
-//     expect(find.text('Description 1'), findsOneWidget);
+    //Verify that the arrows work
+    expect(find.byIcon(Icons.keyboard_arrow_down), findsExactly(2));
+    await tester.tap(find.text('Rule 1'));
+    await tester.pump();
+    expect(find.text('Description 1'), findsOneWidget);
 
-//     //Verify that the "I understand" button works
-//     await tester.tap(find.text('I understand'));
-//     await tester.pump();
-//   });
-// }
+    //Verify that the "I understand" button works
+    await tester.tap(find.text('I understand'));
+    await tester.pump();
+  });
+}
