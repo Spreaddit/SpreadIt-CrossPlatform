82f5e8931ad43243623757abfaa9de63b221ed09
Farida Mukhtar <farida.moukhtar03@eng-st.cu.edu.eg>
2024-04-17 01:07:53 +0200

    updating home unit tests

--- test/homepage/homepage_test.dart
+++ test/homepage/homepage_test.dart
@@ -1,8 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
+import 'package:spreadit_crossplatform/features/homepage/presentation/pages/all.dart';
 import 'package:spreadit_crossplatform/features/homepage/presentation/pages/homepage.dart';
 import 'package:spreadit_crossplatform/features/homepage/presentation/widgets/home_page_drawer.dart';
 import 'package:spreadit_crossplatform/features/homepage/presentation/widgets/post_feed.dart';
+import 'package:spreadit_crossplatform/features/homepage/presentation/widgets/sort_menu.dart';
 
 class CustomBindings extends AutomatedTestWidgetsFlutterBinding {
   @override
@@ -32,5 +34,26 @@ void main() {
     expect(find.byIcon(Icons.account_circle_outlined), findsOneWidget);
     expect(find.byIcon(Icons.bookmarks_outlined), findsOneWidget);
     expect(find.byIcon(Icons.watch_later_outlined), findsOneWidget);
+  });
+  testWidgets('Post feed sorting test (All Page)', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: AllPage()));
+    await tester.pumpAndSettle();
+    // Expect to find SortTypeMenu dialog
+    expect(find.byType(SortTypeMenu), findsOneWidget);
+
+    // Tap on SortTypeMenu to change sort type
+    await tester.tap(find.byType(SortTypeMenu));
+
+    // Wait for the animation to complete
+    await tester.pumpAndSettle();
+
+    // Tap on a different sort type (e.g., Trending)
+    await tester.tap(find.text('hot'));
+
+    // Wait for data to be fetched with the new sort type
+    await tester.pumpAndSettle();
+
+    // Expect to find PostWidget after data is fetched with the new sort type
+    expect(find.text('HOT POSTS'), findsWidgets);
   });
 }
