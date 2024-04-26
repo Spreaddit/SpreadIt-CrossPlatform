ed9ef24d6a615be038f32292ea483fd7ea428588
Farida Mukhtar <farida.moukhtar03@eng-st.cu.edu.eg>
2024-04-26 18:54:26 +0300

    Update floating_new_chat_button.dart

--- lib/features/chat/presentation/widgets/floating_new_chat_button.dart
+++ lib/features/chat/presentation/widgets/floating_new_chat_button.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 Widget floatingNewChatButton(BuildContext context) {
   return FloatingActionButton(
     onPressed: () {
-      Navigator.of(context).pushNamed('new-chat');
+      Navigator.of(context).pushNamed('/new-chat');
       print('Floating button pressed!');
     },
     child: Icon(Icons.chat_bubble),
