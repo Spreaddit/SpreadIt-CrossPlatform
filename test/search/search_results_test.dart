366c4eb156de3a4cc983fd1dfb56c7f0f4104cbc
Chahd ElZalaki <shahd.zalaky@gmail.com>
2024-05-07 20:53:41 +0300

    unit tests

--- test/search/search_results_test.dart
+++ test/search/search_results_test.dart
@@ -1,64 +1,75 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:spreadit_crossplatform/features/homepage/presentation/widgets/post_feed.dart';
+import 'package:spreadit_crossplatform/features/search/presentation/pages/general_search.dart';
 import 'package:spreadit_crossplatform/features/search/presentation/pages/general_search_results.dart';
+import 'package:spreadit_crossplatform/features/search/presentation/pages/page_views/communities_page_view.dart';
 import 'package:spreadit_crossplatform/features/search/presentation/widgets/custom_search_bar.dart';
 import 'package:spreadit_crossplatform/features/search/presentation/widgets/page_view.dart';
+import 'package:spreadit_crossplatform/features/search/presentation/widgets/recent_searches.dart';
 import 'package:spreadit_crossplatform/features/search/presentation/widgets/search_result_header.dart';
+import 'package:spreadit_crossplatform/features/search/presentation/widgets/suggested_results.dart';
+import 'package:spreadit_crossplatform/features/search/presentation/widgets/trending_widgets/trending_menu.dart';
 
 void main() {
-  group('SearchResult Tests', () {
 
-    testWidgets('SearchResult widget renders correctly', (WidgetTester tester) async {
-      final searchItem = 'test_search';
-      await tester.pumpWidget(MaterialApp(home: SearchResult(searchItem: searchItem)));
+  setUpAll(() => HttpOverrides.global = null);
 
-      // Check if CustomSearchBar is present
-      //expect(find.byType(CustomSearchBar), findsOneWidget);
-      //expect(find.text(searchItem), findsOneWidget); // Check if initial search item is displayed
+  group('SearchResults Tests', () {
 
-      // Check if SearchResultHeader is present and displays all labels
-      //expect(find.byType(SearchResultHeader), findsOneWidget);
-      //final labelsList = tester.widget<SearchResultHeader>(find.byType(SearchResultHeader)).labels;
-      //expect(labelsList.length, 5); // Check if all 5 labels are present
+    testWidgets('SearchResults widget renders correctly', (WidgetTester tester) async {
+      final String searchItem = 'search_item';
+      await tester.runAsync(() async {
+        await tester.pumpWidget(MaterialApp(
+          home: Scaffold(
+            body: SearchResult(searchItem: searchItem,),
+          ),
+        ));
+      });
 
+      // Check if intial widgets are present
+      expect(find.byType(CustomSearchBar), findsOneWidget);
+      expect(find.byIcon(Icons.arrow_back), findsOneWidget);
+      //Check if SearchResultHeader is present and displays all labels
+      expect(find.byType(SearchResultHeader), findsOneWidget);
+      // Check if all 5 labels are present
+      final labelsList = tester.widget<SearchResultHeader>(find.byType(SearchResultHeader)).labels;
+      expect(labelsList.length, 5); 
       // Check if CustomPageView is present
-      //expect(find.byType(CustomPageView), findsOneWidget);
+      expect(find.byType(CustomPageView), findsOneWidget);
     });
 
-    testWidgets('Tapping back button navigates back', (WidgetTester tester) async {
-      final searchItem = 'test_search';
-      pushWidget(SearchResult(searchItem: searchItem)); // Use pushWidget to simulate navigation
+    testWidgets('Tapping community label', (WidgetTester tester) async {
+      final String searchItem = 'search_item';
+      await tester.runAsync(() async {
+        await tester.pumpWidget(MaterialApp(
+          home: Scaffold(
+            body: SearchResult(searchItem: searchItem,),
+          ),
+        ));
+      });
 
-      // Simulate tapping back button
-      await tester.tap(find.byIcon(Icons.arrow_back));
-      await tester.pump();
+      final communityLabel = find.text('Communities');
+      await tester.tap(communityLabel);
+      // Find the CustomPageView widget
+      final pageViewFinder = find.byType(CustomPageView);
+      expect(pageViewFinder, findsOneWidget);
+      // Get the pages list from the CustomPageView
+      // final CustomPageView pageViewState = tester.state(pageViewFinder) as CustomPageView;
+      // final List<Widget> pages = pageViewState.pages;
+      // Verify that the second element in the pages list is of type CommunitiesPageView
+      //expect(pages[1], isA<CommunitiesPageView>);
+      //await tester.enterText(searchBarFinder, 'test_search');
 
-      // No need to verify pop with Mockito as we directly navigate
+      // Rebuild the widget tree
+      //
 
-      // You can add assertions here to verify the widget state after navigating back,
-      // for example, checking if the widget is no longer displayed
+      // Verify SuggestedResults are displayed
+      // expect(find.byType(SuggestedResults), findsOneWidget);
+      // expect(find.byType(RecentSearches), findsNothing); 
+      // expect(find.byType(TrendingMenu), findsNothing);
     });
-
-    testWidgets('Selecting a tab in SearchResultHeader calls onTabSelected', (WidgetTester tester) async {
-      final searchItem = 'test_search';
-      pushWidget(SearchResult(searchItem: searchItem)); // Use pushWidget to simulate navigation
-
-      // Select the second tab (index 1)
-      await tester.tap(find.text('Communities'));
-      await tester.pump();
-
-      // Verify that onTabSelected was called with index 1
-      //expect(find.byWidgetPredicate((widget) => widget is SearchResult && widget.selectedIndex == 1), findsOneWidget);
-    });
-
-    // Add more tests to cover other functionalities like searching, navigation within page views, etc.
   });
-}
-
-void pushWidget(Widget widget) {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(widget); // Wrap the widget directly
-  //popUntil((route) => route.isFirst); // Pop until the first route (usually the test app)
 }
