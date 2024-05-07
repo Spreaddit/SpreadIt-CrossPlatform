320efb8017b3bd8bc9b74375676d232ef4b77e7e
Farida Mukhtar <farida.moukhtar03@eng-st.cu.edu.eg>
2024-05-08 01:32:55 +0300

    Update post_feed.dart

--- lib/features/homepage/presentation/widgets/post_feed.dart
+++ lib/features/homepage/presentation/widgets/post_feed.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:spreadit_crossplatform/features/generic_widgets/snackbar.dart';
 import 'package:spreadit_crossplatform/features/homepage/data/get_feed_posts.dart';
 import 'package:spreadit_crossplatform/features/homepage/data/post_class_model.dart';
 import 'package:spreadit_crossplatform/features/homepage/presentation/widgets/post_widget.dart';
