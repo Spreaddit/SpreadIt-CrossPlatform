2ce0d62e70b18316df7d2c12a61e1b39e6552b29
Basem1166 <basem_elgalfy@yahoo.com>
2024-05-07 16:45:16 +0300

    chore: Update API URL in getCommunityInfo function

--- lib/features/community/data/api_community_info.dart
+++ lib/features/community/data/api_community_info.dart
@@ -5,7 +5,7 @@ import 'package:spreadit_crossplatform/user_info.dart';
 /// Retrieves community information from the API endpoint '$communityApiUrlGalal/community/get-info'.
 /// Takes the [String] parameter [communityName]
 ///
-/// Returns a [Map] containing community information including 'avatar', 'email', and 'username'.
+/// Returns a [Map] containing community information.
 ///
 /// Returns a [Map] with default values if fetching fails.
 ///
@@ -14,20 +14,20 @@ Future<Map<String, dynamic>> getCommunityInfo(String communityName) async {
   var defaultResponse = {
     "name": "",
     "category": "",
+    "is18plus": "false",
     "communityType": "Public",
     "description": "",
     "image": "",
-    "memberscount": 0,
-    "rules": [
-      {"title": "", "description": "", "reportReason": ""}
-    ],
+    "membersCount": 0,
+    "rules": [],
     "dateCreated": "",
     "communityBanner": ""
   };
   String? accessToken = UserSingleton().getAccessToken();
   try {
     var response = await Dio().get(
-      '$apiUrl/community/get-info',
+      //TODO: Change the API URL to: '$apiUrl/community/$communityName/get-info'
+      '$apiUrl/community/$communityName/get-info',
       queryParameters: {"communityName": communityName},
       options: Options(
         headers: {
@@ -38,14 +38,15 @@ Future<Map<String, dynamic>> getCommunityInfo(String communityName) async {
     if (response.statusCode == 200) {
       {
         print(response.statusMessage);
-        print('COMMUNITU INFO: ${response.data}');
         return response.data;
       }
     } else {
+      print("kys");
       print('Failed to fetch data. Status code: ${response.statusCode}');
       return defaultResponse;
     }
   } catch (e) {
+    print("kyss");
     print('Error fetching data: $e');
     return defaultResponse;
   }
